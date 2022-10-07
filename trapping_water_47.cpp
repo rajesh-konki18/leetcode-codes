814c37e4fcbad09207d63b39d45ab8332d312d13
Rajesh-Technovert <rajesh.k@technovert.com>
2022-10-07 08:37:34 +0530

    trapping water  - 47.0.1

--- trapping_water_47.cpp
+++ trapping_water_47.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
 #include<vector>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-
+int getMin(int f, int s){
+    return f < s ? f:s;
+}
 int trap(vector<int>& arr){
     vector<int> minArr, maxArr;
     int min = 0, max = 0;
@@ -13,12 +17,27 @@ int trap(vector<int>& arr){
         min = *i;
        }
     }
-    for(auto i = arr.begin(); i != arr.end(); i++){
-       cout<<*i;
+    int arr_size = arr.size();
+    for(auto i = arr_size - 1; i >= 0; i--){
+       maxArr.push_back(max);
+       if(arr[i] > max){
+        max = arr[i];
+       }
     }
-    return 0;
+    reverse(maxArr.begin(), maxArr.end());
+    int sum = 0;
+    for(int i = 0; i <= arr_size - 1; i++){
+            int t = (getMin(minArr[i], maxArr[i]) - arr[i]);
+            if(t>0){
+                sum += t;
+            }
+
+    }
+    return sum;
 
 }
+
+
 
 int main(){
     vector<int> arr;
