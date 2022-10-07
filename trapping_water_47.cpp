3895f43384e272245db51797d8cf292f7efcdad3
Rajesh-Technovert <rajesh.k@technovert.com>
2022-10-07 08:47:18 +0530

    format change

--- trapping_water_47.cpp
+++ trapping_water_47.cpp
@@ -1,45 +1,50 @@
-#include<iostream>
-#include<vector>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int getMin(int f, int s){
-    return f < s ? f:s;
+int getMin(int f, int s)
+{
+    return f < s ? f : s;
 }
-int trap(vector<int>& arr){
+int trap(vector<int> &arr)
+{
     vector<int> minArr, maxArr;
     int min = 0, max = 0;
 
-    for(auto i = arr.begin(); i != arr.end(); i++){
-       minArr.push_back(min);
-       if(*i > min){
-        min = *i;
-       }
+    for (auto i = arr.begin(); i != arr.end(); i++)
+    {
+        minArr.push_back(min);
+        if (*i > min)
+        {
+            min = *i;
+        }
     }
     int arr_size = arr.size();
-    for(auto i = arr_size - 1; i >= 0; i--){
-       maxArr.push_back(max);
-       if(arr[i] > max){
-        max = arr[i];
-       }
+    for (auto i = arr_size - 1; i >= 0; i--)
+    {
+        maxArr.push_back(max);
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
     }
     reverse(maxArr.begin(), maxArr.end());
     int sum = 0;
-    for(int i = 0; i <= arr_size - 1; i++){
-            int t = (getMin(minArr[i], maxArr[i]) - arr[i]);
-            if(t>0){
-                sum += t;
-            }
-
+    for (int i = 0; i <= arr_size - 1; i++)
+    {
+        int t = (getMin(minArr[i], maxArr[i]) - arr[i]);
+        if (t > 0)
+        {
+            sum += t;
+        }
     }
     return sum;
-
 }
 
-
-
-int main(){
+int main()
+{
     vector<int> arr;
     arr.push_back(4);
     arr.push_back(2);
@@ -47,6 +52,6 @@ int main(){
     arr.push_back(3);
     arr.push_back(2);
     arr.push_back(5);
-    cout<<trap(arr);
+    cout << trap(arr);
     return 0;
 }
